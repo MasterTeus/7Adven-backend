afd18af5f9a0d51914b0952946a1b61443d74bc4
MasterTeus <mateus.cazuza@hotmail.com>
2022-01-05 00:05:56 -0300

    initial setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import { routes } from "./routes.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+mongoose.connect(
+  "mongodb+srv://admin:7adven2022@cluster0.cbv5j.gcp.mongodb.net/AdvenDB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.listen(3333);
