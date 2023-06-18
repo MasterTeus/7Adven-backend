1a97bafc13ae23afc99b7aa3732609292cd42461
Mateus Cazuza <mateus.cazuza@hotmail.com>
2023-06-18 15:09:28 -0300

    update route

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
-const { Router } = require("express");
-const ProjectController = require("./controllers/ProjectController");
-const GuestController = require("./controllers/GuestController");
+const { Router } = require('express');
+const ProjectController = require('./controllers/ProjectController');
+const GuestController = require('./controllers/GuestController');
 // const UserController = require("./controllers/UserController");
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Ola Mundo' });
+});
+
 // routes.post("/user", UserController.store);
 // routes.get("/user", UserController.indexDeviceId);
 
-routes.post("/project", ProjectController.store);
-routes.get("/project/:id", ProjectController.index);
-routes.post("/add-contributors/:id", ProjectController.storeContributors);
+routes.post('/project', ProjectController.store);
+routes.get('/project/:id', ProjectController.index);
+routes.post('/add-contributors/:id', ProjectController.storeContributors);
 
-routes.post("/guest", GuestController.store);
-routes.get("/guests-to-project/:id", GuestController.getGuestForProject);
-routes.post("/checking-presence/:id/", GuestController.checkingPresence);
-routes.post("/checking-visited/:id/", GuestController.checkingVisited);
-routes.get("/get-guest/:hash/", GuestController.getGuestHash);
+routes.post('/guest', GuestController.store);
+routes.get('/guests-to-project/:id', GuestController.getGuestForProject);
+routes.post('/checking-presence/:id/', GuestController.checkingPresence);
+routes.post('/checking-visited/:id/', GuestController.checkingVisited);
+routes.get('/get-guest/:hash/', GuestController.getGuestHash);
 
 module.exports = routes;
